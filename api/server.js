3c4b043bcf8c1b4a47890cc58eabba10138ad772
LucasOliveira2077 <lucasdeoliveira.95@outlook.com>
2022-03-28 23:46:09 -0300

    new feature

--- api/server.js
+++ api/server.js
@@ -11,8 +11,19 @@ app.use(bodyParser.json());
 let port = 8080;
 app.listen(port);
 
+let db = new mongodb.db(
+    'instagram',
+    new mongo.Server('localhost', 27017, {})
+    
+);
+
 console.log('o servidor esta funcionando');
 
 app.get('/', (req, res) => {
     res.send({mensagem: 'E ai tudo bem?'});
+});
+
+app.post('/api', (req, res) => {
+    let dados = req.body;//os parametros de bodyparser serao carregados nesse body
+    res.send(dados);
 });
